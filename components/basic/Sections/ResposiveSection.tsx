16f6f892adfccb8d5d61cbc89baef8501ce08061
Colin Rumball <colin.rumball@gmail.com>
2020-02-28 01:36:18 -0500

    Added padding to responsive sections on small screens

--- components/basic/Sections/ResposiveSection.tsx
+++ components/basic/Sections/ResposiveSection.tsx
@@ -9,13 +9,12 @@ interface StyledResposiveSectionProps {}
 const StyledResposiveSection = styled.section<StyledResposiveSectionProps>`
    display: flex;
    flex-direction: column;
-
+   padding: ${({ theme }: ThemeContainer) => theme.VARIABLES.SPACING.L} 0;
    max-width: 100%;
 
    @media (min-width: ${({ theme }: ThemeContainer) => theme.VARIABLES.BREAK_POINTS.MEDIUM}) {
       flex-direction: row;
       justify-content: space-evenly;
-      padding: ${({ theme }: ThemeContainer) => theme.VARIABLES.SPACING.L} 0;
    }
 `;
 
